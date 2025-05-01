32eff30f9035263edff2ac6b87306e065ccff952
hyeony <165357087+hyeonybraincloud@users.noreply.github.com>
2025-05-01 15:26:04 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -4,34 +4,37 @@
 using namespace std;
 
 int dfs(int node, const vector<vector<int>>& graph, vector<bool>& visited) {
-    visited[node] = true;
-    int count = 0;
+	visited[node] = true;
+	int count = 0;
 
-    for (int neighbor : graph[node]) {
-        if (!visited[neighbor]) {
-            count += 1 + dfs(neighbor, graph, visited);
-        }
-    }
+	for (int idx = 1; idx <= graph.size(); idx++) {
+		if (!visited[idx] && graph[node][idx] == 1)
+			count += (1 + dfs(idx, graph, visited));
+	}
 
-    return count;
+	return count;
 }
 
 int main() {
-    int n, m;
-    cin >> n >> m;
+	int n, m;
 
-    vector<vector<int>> graph(n + 1);      // 1번부터 n번까지 사용
-    vector<bool> visited(n + 1, false);    // 방문 체크
+	cin >> n >> m;
 
-    for (int i = 0; i < m; i++) {
-        int a, b;
-        cin >> a >> b;
-        graph[a].push_back(b); // 양방향 연결
-        graph[b].push_back(a);
-    }
+	vector<vector<int>> graph(n + 1, vector<int>(n + 1, 0));
+	vector<bool> visited(n, false);
 
-    int result = dfs(1, graph, visited);  // 1번 컴퓨터에서 감염 시작
-    cout << result << endl;
+	for (int idx = 0; idx < m; idx++) {
+		int a, b;
+		
+		cin >> a >> b;
 
-    return 0;
+		graph[a][b] = 1;
+		graph[b][a] = 1;
+	}
+
+	int result = dfs(1, graph, visited);
+
+	cout << result << endl;
+
+	return 0;
 }
